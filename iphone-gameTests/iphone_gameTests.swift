93d14ae2b7e8f9df348bc284946162cd474ef588
高橋音穏 <takahashineo@neo-macbook-air.local>
2018-03-06 17:14:01 +0900

    Initial Commit

---
+++ iphone-gameTests/iphone_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  iphone_gameTests.swift
+//  iphone-gameTests
+//
+//  Created by 高橋　音穏 on 2018/03/06.
+//  Copyright © 2018年 mesuki. All rights reserved.
+//
+
+import XCTest
+@testable import iphone_game
+
+class iphone_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
